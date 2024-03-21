0db8f43cb289004d1eac6133602d4b07a4a5ff73
Vasilii <pimenovasya@gmail.com>
2024-03-21 23:27:53 +0300

    Fixed DateTime formatting

--- BGKutaisiBot/UI/Commands/SendNotice.cs
+++ BGKutaisiBot/UI/Commands/SendNotice.cs
@@ -1,6 +1,7 @@
 ﻿using BGKutaisiBot.Types;
 using Telegram.Bot;
 using Telegram.Bot.Types.Enums;
+using System.Globalization;
 
 namespace BGKutaisiBot.UI.Commands
 {
@@ -18,7 +19,7 @@ namespace BGKutaisiBot.UI.Commands
 
 			async Task Function(string[] args)
 			{
-				await new TextMessage(string.Format(TEXT_MESSAGE, DateTime.Now.ToString("dMMMMyyyy"),
+				await new TextMessage(string.Format(TEXT_MESSAGE, DateTime.Now.ToString("dMMMMyyyy", CultureInfo.GetCultureInfo("ru-RU")),
 					Environment.GetEnvironmentVariable("NOTICE_FAQ_MESSAGE_ID") ?? "0", args.Length == 1 ? "" : $"и открытый [опрос по играм](t.me/bg\\_kutaisi/{args[1]}) ")) 
 					{ ParseMode = ParseMode.MarkdownV2 }.SendTextMessageAsync(args[0], this.BotClient);
 			}
