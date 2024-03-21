4d95207beaae5e7447e5de8adea8d6a68dad8dcf
Vasilii <pimenovasya@gmail.com>
2024-03-21 23:28:54 +0300

    Added third argument to UI-command SendNotice

--- BGKutaisiBot/UI/Commands/SendNotice.cs
+++ BGKutaisiBot/UI/Commands/SendNotice.cs
@@ -1,5 +1,6 @@
 ﻿using BGKutaisiBot.Types;
 using Telegram.Bot;
+using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using System.Globalization;
 
@@ -11,21 +12,34 @@ namespace BGKutaisiBot.UI.Commands
 		{
 			const string TEXT_MESSAGE =
 				  "🏆  [Настольные игры](t.me/kutaisi\\_offline\\_rus/5077) за 5₾ \\#цена\r\n"
-				+ "🗓️  Сегодня \\#{0} c 19:00 до 00:00\r\n"
+				+ "🗓️  Сегодня \\#{0} c {1} до 00:00\r\n"
 				+ "[📍](goo.gl/maps/LfxoBVq7ytk4ZdP97)  Клуб настольных игр [Summer Set](t.me/summersetkutaisi) — [Google Maps](goo.gl/maps/LfxoBVq7ytk4ZdP97)\r\n\r\n"
 
 				+ "Обязательно ставьте 👍, если планируете присоединится\r\n"
-				+ "[Ответы на F\\.A\\.Q\\.](t.me/bg\\_kutaisi/{1}) {2}в канале [BGK](t.me/bg\\_kutaisi)";
+				+ "[Ответы на F\\.A\\.Q\\.](t.me/bg\\_kutaisi/{2}) {3}в канале [BGK](t.me/bg\\_kutaisi)";
 
 			async Task Function(string[] args)
 			{
-				await new TextMessage(string.Format(TEXT_MESSAGE, DateTime.Now.ToString("dMMMMyyyy", CultureInfo.GetCultureInfo("ru-RU")),
-					Environment.GetEnvironmentVariable("NOTICE_FAQ_MESSAGE_ID") ?? "0", args.Length == 1 ? "" : $"и открытый [опрос по играм](t.me/bg\\_kutaisi/{args[1]}) ")) 
-					{ ParseMode = ParseMode.MarkdownV2 }.SendTextMessageAsync(args[0], this.BotClient);
+				ChatId chatId = args[0];
+				const string START_TIME = "19:00";
+				TimeOnly startTime = TimeOnly.Parse(START_TIME);
+				int pollMessageId = default;
+				for (int i = 1; i < args.Length; i++)
+					if (TimeOnly.TryParse(args[i], out TimeOnly parsedTime))
+						startTime = startTime.ToString() == START_TIME ? parsedTime
+							: throw new ArgumentException($"Два или более аргумента ({startTime}, {parsedTime}) могут быть интерпретированы как время начала");
+					else if (int.TryParse(args[i], out int parsedMessageId))
+						pollMessageId = pollMessageId == default ? parsedMessageId
+							: throw new ArgumentException($"Два или более аргумента ({pollMessageId}, {parsedMessageId}) могут быть интерпретированы как идентификатор сообщения с опросом");
+
+				await new TextMessage(string.Format(TEXT_MESSAGE, DateTime.Now.ToString("dMMMMyyyy", CultureInfo.GetCultureInfo("ru-RU")), startTime,
+					Environment.GetEnvironmentVariable("NOTICE_FAQ_MESSAGE_ID") ?? "0", pollMessageId == default ? "" : $"и открытый [опрос по играм](t.me/bg\\_kutaisi/{pollMessageId}) "))
+				{ ParseMode = ParseMode.MarkdownV2 }.SendTextMessageAsync(chatId, this.BotClient);
 			}
 
 			this.Add(1, Function);
 			this.Add(2, Function);
+			this.Add(3, Function);
 		}
 	}
 }
