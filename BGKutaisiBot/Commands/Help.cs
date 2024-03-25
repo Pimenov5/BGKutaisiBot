6bbb974fe09ab8aa875cdf6dd6221c6d456fc2fd
Vasilii <pimenovasya@gmail.com>
2024-03-25 14:55:58 +0300

    Added class Help

---
+++ BGKutaisiBot/Commands/Help.cs
@@ -0,0 +1,14 @@
+﻿namespace BGKutaisiBot.Commands
+{
+	internal class Help
+	{
+		public static void Respond()
+		{
+			IEnumerable<Type> types = typeof(Help).Assembly.GetTypes().Where((Type type) => type.Namespace == typeof(Help).Namespace);
+			foreach (Type type in types)
+				if (type.GetProperty("Description", typeof(string))?.GetValue(null) is string description)
+					Console.WriteLine(type.Name.ToLower() + $" - {description}");
+			Console.WriteLine();
+		}
+	}
+}
