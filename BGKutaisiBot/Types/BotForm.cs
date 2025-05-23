7cdf11a794d6db19a6e4d5ff7e001add91f52dfa
Vasilii <pimenovasya@gmail.com>
2025-05-23 11:04:52 +0300

    Added internal class BotForm

---
+++ BGKutaisiBot/Types/BotForm.cs
@@ -0,0 +1,8 @@
+﻿namespace BGKutaisiBot.Types
+{
+	internal abstract class BotForm : BotCommand
+	{
+		private bool _isCompleted = false;
+		public bool IsCompleted { get { return _isCompleted; } protected set { _isCompleted = value; } }
+	}
+}
