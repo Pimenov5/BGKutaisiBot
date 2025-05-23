2d97aff3632b5033c70bf6a6593483e72ef7b829
Vasilii <pimenovasya@gmail.com>
2025-05-23 15:11:14 +0300

    Fixed public static methods RespondAsync

--- BGKutaisiBot/BotCommands/StopPoll.cs
+++ BGKutaisiBot/BotCommands/StopPoll.cs
@@ -1,4 +1,5 @@
-﻿using BGKutaisiBot.Types.Logging;
+﻿using BGKutaisiBot.Attributes;
+using BGKutaisiBot.Types.Logging;
 using Telegram.Bot;
 using Telegram.Bot.Types;
 
@@ -6,22 +7,25 @@ namespace BGKutaisiBot.BotCommands
 {
 	internal class StopPoll : Types.BotCommand
 	{
-		public static string Description => "Остановить опрос";
-
-		public override string[] GetArguments(Message message) => message.ReplyToMessage is Message replyToMessage && replyToMessage.Poll is Poll poll && !poll.IsClosed
-			? [replyToMessage.Chat.Id.ToString(), replyToMessage.MessageId.ToString()] : throw new ArgumentException("Команда должна вызываться в ответ на незакрытый опрос");
+		public override string[] GetArguments(Message message) => message.ReplyToMessage is Message replyToMessage && replyToMessage.Poll is Poll poll && !poll.IsClosed && message.From?.Id is long userId
+			? [userId.ToString(), replyToMessage.Chat.Id.ToString(), replyToMessage.MessageId.ToString()] : throw new ArgumentException("Команда должна вызываться в ответ на незакрытый опрос");
 
 		public static async Task RespondAsync(ITelegramBotClient botClient, string chatId, string messageId, CancellationToken cancellationToken)
 		{
 			await botClient.StopPollAsync(chatId, int.Parse(messageId), cancellationToken: cancellationToken);
 			Logs.Instance.Add($"Остановлен опрос в чате ID {chatId} в сообщении ID {messageId}");
 		}
-		public static async Task RespondAsync(ITelegramBotClient botClient, string chatId, string messageId) => await RespondAsync(botClient, chatId, messageId, default);
+		public static async Task RespondAsync(ITelegramBotClient botClient, string userId, string chatId, string messageId)
+		{
+			if (!Admin.Contains(long.Parse(userId)))
+				throw new ArgumentException("Только администраторы могут останавливать опросы");
+			await RespondAsync(botClient, chatId, messageId, (CancellationToken)default);
+		}
 		public static async Task RespondAsync(ITelegramBotClient botClient, string[] args)
 		{
-			if (args.Length != 2)
+			if (args.Length != 3)
 				throw new ArgumentException("Требуется ID чата и сообщения с опросом");
-			await RespondAsync(botClient, args[0], args[1]);
+			await RespondAsync(botClient, args[0], args[1], args[2]);
 		}
 	}
 }
